0821c5d7f40a5acc0abf493ee6a952d24e45ac6a
AnupamaCodippily <avcodippily@gmail.com>
2023-01-20 20:39:04 +0530

    remove dead code

--- src/state/queries/chatQueries.ts
+++ src/state/queries/chatQueries.ts
@@ -26,18 +26,6 @@ export const api = createApi({
           // wait for the initial query to resolve before proceeding
           await cacheDataLoaded
 
-          // when data is received from the socket connection to the server,
-          // if it is a message and for the appropriate channel,
-          // update our query result with the received message
-          const listener = (event: MessageEvent) => {
-            const data = JSON.parse(event.data)
-            // if (!isMessage(data) || data.channel !== arg) return
-
-            updateCachedData((draft) => {
-              draft.push(data)
-            })
-          }
-
           setupSocketListeners(socket);
         } catch {
           // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
