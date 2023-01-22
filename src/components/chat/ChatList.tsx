7ea52901999c8ccc21ddf0871af76a0ff189f454
AnupamaCodippily <avcodippily@gmail.com>
2023-01-22 22:19:02 +0530

    remove unused dispatch

--- src/components/chat/ChatList.tsx
+++ src/components/chat/ChatList.tsx
@@ -1,25 +1,29 @@
-import React, { ReactNode, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { api } from '../../state/queries/chatQueries'
-import { IChatMessageComponent } from './ChatMessage'
+import React, { ReactNode, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { api } from "../../state/queries/chatQueries";
+import { RootState } from "../../state/store";
+import IChatMessage from "../../types/Message";
+import ChatMessage, { IChatMessageComponent } from "./ChatMessage";
 
 interface IChatListComponent {
-  messages?: ReactNode[]
+  messages?: ReactNode[];
 }
 
 const ChatList: React.FC<IChatListComponent> = ({ messages = [] }) => {
+  api.endpoints.getMessages.useQuery("blah");
 
-  const dispatch = useDispatch();
-  const x = api.endpoints.getMessages.useQuery('blah');
-  useEffect(() => {
-  console.log(x)
-  }, [x]);
+  const messagesList = useSelector(
+    (state: RootState) => state.chat.chatMessages
+  );
 
   return (
-    <div className='chat-list'>
-        {  messages }
+    <div className="chat-list">
+      {messagesList.length}
+      {messagesList.map((m: IChatMessage) => (
+        <ChatMessage chatMessage={m.body} senderName={m.username} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ChatList
+export default ChatList;
