09dfd8cfc96aa32df519108d1110a36dedb53b90
AnupamaCodippily <avcodippily@gmail.com>
2023-02-08 01:02:29 +0530

    chatbox styling

--- src/components/chat/Chatbox.tsx
+++ src/components/chat/Chatbox.tsx
@@ -1,31 +1,41 @@
-import React, { FormEvent, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { api } from '../../state/queries/chatQueries'
-
-const Chatbox :React.FC = () => {
-
-  const username = 'test user'
-  const [message, setMessage] = useState('');
+import React, { FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { api } from "../../state/queries/chatQueries";
+import sendIcon from "../../assets/icons/send-msg-icon.png";
+const Chatbox: React.FC = () => {
+  const username = "test user";
+  const [message, setMessage] = useState("");
 
   const dispatch = useDispatch();
 
   function handleCreateMessage(event: FormEvent) {
-
     event.preventDefault();
-    
-    dispatch(api.endpoints.sendMessage.initiate({ username, body: message}))
 
-    setMessage(_ => "")
+    dispatch(api.endpoints.sendMessage.initiate({ username, body: message }));
+
+    setMessage((_) => "");
   }
 
   return (
-    <div className='chat-box'>
+    <div className="chat-box-outer">
+      <div className="chat-box">
         <form onSubmit={(e) => handleCreateMessage(e)}>
-            <input type='text' className='chatbox-input' value={message} onChange={(e) => setMessage(_ => e.target.value)}/>
-            <input type='submit' />
+          <div className="chatbox-input-container">
+            <input
+              type="text"
+              className="chatbox-input"
+              value={message}
+              onChange={(e) => setMessage((_) => e.target.value)}
+              placeholder='Type your message'
+            />
+            <button>
+              <img src={sendIcon} alt="send message" />
+            </button>
+          </div>
         </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chatbox
+export default Chatbox;
