8ed32ef91946cc2e97eeeb167973efe23424afcd
AnupamaCodippily <avcodippily@gmail.com>
2023-02-12 02:58:05 +0530

    add test log

--- src/lib/webrtc/create-peerjs-connection.ts
+++ src/lib/webrtc/create-peerjs-connection.ts
@@ -1,24 +1,23 @@
+import { randomUUID } from "crypto";
 import Peer from "peerjs";
 import { store } from "../../state/store";
 import { getLocalStream } from "./setup-media-sources";
 
 let clientPeer: Peer | null = null;
-export let peerId = 'empty';
+export let peerId = randomUUID();
+
 export function getPeer() {
-    if (clientPeer) return clientPeer;
+  if (clientPeer) return clientPeer;
 
-    clientPeer = new Peer()
+  clientPeer = new Peer(peerId);
 
-    clientPeer.on('open', id => peerId = id)
+  clientPeer.on("call", async (call) => {
+    if (!store.getState().room.isHost) {
+      const ls = await getLocalStream();
+      alert("Answering call");
+      call.answer(ls);
+    }
+  });
 
-    clientPeer.on('call', async (call) => {
-        if (!store.getState().room.isHost) {
-            const ls = await getLocalStream();
-            alert('Answering call')
-            call.answer(ls);
-        }
-    })
-
-    return clientPeer;
+  return clientPeer;
 }
-
