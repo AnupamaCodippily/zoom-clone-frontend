333424fd7988066aa9684a31b1b0b23f8a7cb9a0
AnupamaCodippily <avcodippily@gmail.com>
2023-02-12 11:54:42 +0530

    add ice server and config list

--- src/lib/webrtc/create-peerjs-connection.ts
+++ src/lib/webrtc/create-peerjs-connection.ts
@@ -3,13 +3,14 @@ import { store } from "../../state/store";
 import { getLocalStream } from "./setup-media-sources";
 import { v4, v5 } from "uuid";
 import { setDisplayingRemoteStream } from "../../state/slices/room";
+import servers from "../constants/ice-servers";
 let clientPeer: Peer | null = null;
 export let peerId = v4();
 
 export function getPeer() {
   if (clientPeer) return clientPeer;
 
-  clientPeer = new Peer(peerId);
+  clientPeer = new Peer(peerId, { config: { iceServers: servers.iceServers } });
 
   clientPeer.on("call", async (call) => {
     if (!store.getState().room.isHost) {
