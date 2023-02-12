be43210ef27d0056d99e611d7b4f22c0c4c81614
AnupamaCodippily <avcodippily@gmail.com>
2023-02-12 13:37:01 +0530

    fix method name

--- src/lib/webrtc/create-peerjs-connection.ts
+++ src/lib/webrtc/create-peerjs-connection.ts
@@ -2,7 +2,7 @@ import Peer from "peerjs";
 import { store } from "../../state/store";
 import { getLocalStream } from "./setup-media-sources";
 import { v4 } from "uuid";
-import { setDisplayingRemoteStream } from "../../state/slices/room";
+import { setDisplayingRemoteStream, setRemoteDisplayStream } from "../../state/slices/room";
 import servers from "../constants/ice-servers";
 let clientPeer: Peer | null = null;
 export let peerId = v4();
@@ -20,7 +20,7 @@ export function getPeer() {
 
     call.on('stream', function(stream) {
         // `stream` is the MediaStream of the remote peer.
-        store.dispatch(setDisplayingRemoteStream(stream))
+        store.dispatch(setRemoteDisplayStream(stream))
       });
 
       store.dispatch(setDisplayingRemoteStream(true));
