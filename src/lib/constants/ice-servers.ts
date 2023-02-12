a83f3c71be19570cb858fca6fc1653ddd6cf10c1
AnupamaCodippily <avcodippily@gmail.com>
2023-02-12 11:47:57 +0530

    add temporary turn servers

--- src/lib/constants/ice-servers.ts
+++ src/lib/constants/ice-servers.ts
@@ -3,6 +3,11 @@ const servers = {
     {
       urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
     },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credentials: 'openrelayproject'
+  }
   ],
   iceCandidatePoolSize: 10,
 };
