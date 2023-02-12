731bd66dc7f8195c628251b3204bc46742cc829b
AnupamaCodippily <avcodippily@gmail.com>
2023-02-12 12:34:21 +0530

    add custom turn server (temporary, use your own!)

--- src/lib/constants/ice-servers.ts
+++ src/lib/constants/ice-servers.ts
@@ -1,13 +1,23 @@
 const servers = {
   iceServers: [
     {
-      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+      urls: "stun:relay.metered.ca:80",
     },
     {
-      urls: 'turn:openrelay.metered.ca:80',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
-  }
+      urls: "turn:relay.metered.ca:80",
+      username: "8e360a028e42554dd5f74d4c",
+      credential: "R7ZU/iChDH4jPqHc",
+    },
+    {
+      urls: "turn:relay.metered.ca:443",
+      username: "8e360a028e42554dd5f74d4c",
+      credential: "R7ZU/iChDH4jPqHc",
+    },
+    {
+      urls: "turn:relay.metered.ca:443?transport=tcp",
+      username: "8e360a028e42554dd5f74d4c",
+      credential: "R7ZU/iChDH4jPqHc",
+    },
   ],
   iceCandidatePoolSize: 10,
 };
