2343ccec3a59f66e45098a9d51b329648edea060
AnupamaCodippily <avcodippily@gmail.com>
2023-02-12 13:48:28 +0530

    make call sync

--- src/lib/webrtc/create-peerjs-connection.ts
+++ src/lib/webrtc/create-peerjs-connection.ts
@@ -2,7 +2,10 @@ import Peer from "peerjs";
 import { store } from "../../state/store";
 import { getLocalStream } from "./setup-media-sources";
 import { v4 } from "uuid";
-import { setDisplayingRemoteStream, setRemoteDisplayStream } from "../../state/slices/room";
+import {
+  setDisplayingRemoteStream,
+  setRemoteDisplayStream,
+} from "../../state/slices/room";
 import servers from "../constants/ice-servers";
 let clientPeer: Peer | null = null;
 export let peerId = v4();
@@ -12,15 +15,13 @@ export function getPeer() {
 
   clientPeer = new Peer(peerId, { config: { iceServers: servers.iceServers } });
 
-  clientPeer.on("call", async (call) => {
+  clientPeer.on("call", (call) => {
     if (!store.getState().room.isHost) {
-      const ls = await getLocalStream();
       alert("Answering call");
-    //   call.answer(ls);
 
-    call.on('stream', function(stream) {
+      call.on("stream", function (stream) {
         // `stream` is the MediaStream of the remote peer.
-        store.dispatch(setRemoteDisplayStream(stream))
+        store.dispatch(setRemoteDisplayStream(stream));
       });
 
       store.dispatch(setDisplayingRemoteStream(true));
